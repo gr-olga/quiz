2f24dbf9763f0e5f6302b4efa63f7e3256583778
Olga Panfilova <gr-olga@ya.ru>
2024-07-18 15:44:30 +0200

    fix type add selects

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import './App.css'
 import {QuestionsComponent} from "./components/QuizQuestions.tsx";
-import {useEffect} from "react";
-import {fetchQuestions} from "./api/api.ts";
+import {useEffect, useState} from "react";
+import {fetchCategories, fetchQuestions} from "./api/api.ts";
 import {fetchQuestionsSuccess} from "./store/quizStore.ts";
 import {AppDispatch} from "./store";
 import {useDispatch} from "react-redux";
-import {IQuestion} from "./types/quizTypes.ts";
+import {IQuestion, QuestionType, QuizCategory, QuizLevel} from "./types/quizTypes.ts";
 
 function App() {
     const dispatch: AppDispatch = useDispatch();
+    const [category, setCategory] = useState<number | undefined>(undefined);
+    const [difficulty, setDifficulty] = useState<QuizLevel | undefined>(undefined);
+    const [type, setType] = useState<QuestionType | undefined>(undefined);
+
+    const [allCategories, setAllCategories] = useState<ReadonlyArray<QuizCategory>>([]);
+
     useEffect(() => {
-        fetchQuestions( undefined,undefined, 'multiple').then((data: ReadonlyArray<IQuestion>) => {
-            console.log('data', data);
+        fetchQuestions(category, difficulty, type).then((data: ReadonlyArray<IQuestion>) => {
+            console.log('dataApp', data);
             dispatch(fetchQuestionsSuccess(data));
         })
 
-    }, [dispatch]);
+    }, [dispatch, category, difficulty, type]);
+
+    useEffect(() => {
+        fetchCategories().then((data) => setAllCategories(data));
+    }, []);
+
 
     return (
         <>
-            <h1>Quiz</h1>
-            <QuestionsComponent/>
+            <div>
+                <form onSubmit={}>
+                    <h2>Select Category</h2>
+                    <select onChange={(e) => setCategory(e.target.value as number)}>
+                        <option value={undefined}>Any Category</option>
+                        {allCategories.map((category: QuizCategory) => <option key={category.id}
+                                                                               value={category.id}>{category.name}</option>)}
+                    </select>
+                    <h2>Select Difficulty</h2>
+                    <select onChange={(e) => setDifficulty(e.target.value as QuizLevel)}>
+                        <option value={undefined}>Any Difficulty</option>
+                        <option value='easy'>Easy</option>
+                        <option value='medium'>Medium</option>
+                        <option value='hard'>Hard</option>
+                    </select>
+                    <h2>Select Type</h2>
+                    <select onChange={(e) => setType(e.target.value as QuestionType)}>
+                        <option value={undefined}>Any Type</option>
+                        <option value='multiple'>Multiple Choice</option>
+                        <option value='boolean'>True/False</option>
+                    </select>
+                    <button type='submit'>Start Quiz</button>
+                </form>
+            </div>
+            <div>
+                <h1>Quiz</h1>
+                <QuestionsComponent/>
+            </div>
         </>
     )
 }
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosResponse} from 'axios';
-import {IQuestion, QuestionType, QuizLevel} from "../types/quizTypes.ts";
+import {IQuestion, QuestionType, QuizCategory, QuizLevel} from "../types/quizTypes.ts";
 
 const BASE_URL = 'https://opentdb.com/api.php?amount=10';
 
@@ -7,8 +7,19 @@ export const fetchQuestions = async (category?: number, difficulty?: QuizLevel, 
     try {
         const url = `${BASE_URL}${category ? `&category=${category}` : ''}${difficulty ? `&difficulty=${difficulty}` : ''}${type ? `&type=${type}` : ''}`;
         const res: AxiosResponse<{ results: IQuestion[] }> = await axios.get(url);
-        console.log('api', res.data);
         return res.data.results;
+    } catch (e) {
+        console.log('Error:', e);
+    }
+}
+
+export const fetchCategories = async () => {
+    try {
+        const res: AxiosResponse<{
+            trivia_categories: ReadonlyArray<QuizCategory>
+        }> = await axios.get('https://opentdb.com/api_category.php');
+        console.log('api', res.data.trivia_categories);
+        return res.data.trivia_categories;
     } catch (e) {
         console.log('Error:', e);
     }
