1b008ad73bbbdecb5e9b273f92602ae27c88507c
Olga Panfilova <gr-olga@ya.ru>
2024-07-21 12:42:34 +0200

    handle api error

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,12 +5,17 @@ const BASE_URL: string = 'https://opentdb.com/api.php?amount=10';
 
 export const fetchQuestions = async (category?: number, difficulty?: QuizLevel, type?: QuestionType): Promise<ReadonlyArray<Question>> => {
   const url: string = `${BASE_URL}${category ? `&category=${category}` : ''}${difficulty ? `&difficulty=${difficulty}` : ''}${type ? `&type=${type}` : ''}`;
-  const res: AxiosResponse<{ results: Question[] }> = await axios.get(url);
+  const res: AxiosResponse<{ results: Question[] }> = await axios.get(url).catch((error) => {
+    alert('Error fetching questions. Please try again later.');
+    return error;
+  });
   return res.data.results;
 };
 
 export const fetchCategories = async (): Promise<ReadonlyArray<QuizCategory>> => {
-  const res: AxiosResponse<{ trivia_categories: ReadonlyArray<QuizCategory> }> = await axios.get('https://opentdb.com/api_category.php');
-  // console.log('Categories:', res.data.trivia_categories);
+  const res: AxiosResponse<{ trivia_categories: ReadonlyArray<QuizCategory> }> = await axios.get('https://opentdb.com/api_category.php').catch((error) => {
+    alert('Error fetching categories. Please try again later.');
+    return error;
+  });
   return res.data.trivia_categories;
 };
